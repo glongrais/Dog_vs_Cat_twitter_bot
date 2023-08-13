71be4ce8726e8de90834819a88307c3da67aedcb
Guillaume Longrais <guillaume.longrais@orange.fr>
2023-08-13 16:06:47 +0200

    Added more tests

--- tests.py
+++ tests.py
@@ -1,23 +1,21 @@
 import pytest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, call, patch, ANY
 from main import *
 
-# Mock fixture for httpx
+# Mock fixtures
 @pytest.fixture
-def httpx_mock():
+def mock_httpx_client_get():
     with patch('httpx.Client.get') as mock_get:
         yield mock_get
 
-# Mock fixture for tweepy
 @pytest.fixture
-def tweepy_mock():
+def mock_tweepy_client():
     with patch('tweepy.Client.create_tweet') as mock_create_tweet, \
          patch('tweepy.API.media_upload') as mock_media_upload:
         yield mock_create_tweet, mock_media_upload
 
-# Mock fixture for pymysql
 @pytest.fixture
-def pymysql_mock():
+def mock_pymysql_connect():
     with patch('pymysql.connect') as mock_connect:
         mock_db = Mock()
         mock_db.cursor.return_value = Mock()
@@ -25,19 +23,19 @@ def pymysql_mock():
         yield mock_connect
 
 @pytest.fixture
-def requests_mock():
+def mock_requests_get():
     with patch('requests.get') as mock_request:
         yield mock_request
 
 @pytest.fixture
-def db_mock():
+def mock_db_connect():
     with patch('main.db_connect') as mock_connect:
         mock_db = Mock()
         mock_db.cursor.return_value = Mock()
         mock_connect.return_value = mock_db
         yield mock_connect
 
-def test_get_poll_result(httpx_mock):
+def test_get_poll_result(mock_httpx_client_get):
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {
@@ -48,59 +46,103 @@ def test_get_poll_result(httpx_mock):
             }
         }
     }
-    httpx_mock.return_value = mock_response
+    mock_httpx_client_get.return_value = mock_response
 
     dog_count, cat_count = get_poll_result('1234567890')
 
     assert dog_count == 12
     assert cat_count == 10
 
-def test_get_last_poll_id(pymysql_mock):
+def test_get_last_poll_id(mock_pymysql_connect):
     mock_cursor = Mock()
     mock_cursor.fetchone.return_value = [1234567890]
-    pymysql_mock.cursor.return_value = mock_cursor
+    mock_pymysql_connect.cursor.return_value = mock_cursor
 
-    last_poll_id = get_last_poll_id(pymysql_mock)
+    last_poll_id = get_last_poll_id(mock_pymysql_connect)
 
     assert last_poll_id == 1234567890
 
-def test_update_poll(pymysql_mock):
+def test_update_poll(mock_pymysql_connect):
     mock_cursor = Mock()
-    pymysql_mock.cursor.return_value = mock_cursor
+    mock_pymysql_connect.cursor.return_value = mock_cursor
 
-    update_poll(pymysql_mock, 1234567890, 12, 10)
+    update_poll(mock_pymysql_connect, 1234567890, 12, 10)
 
     mock_cursor.execute.assert_called_once_with(
         'UPDATE Polls SET dog_votes = %s, cat_votes = %s, winner = %s  WHERE poll_id = %s',
         (12, 10, 'Dog', 1234567890)
     )
 
-def test_get_total_number_polls(pymysql_mock):
+def test_get_total_number_polls(mock_pymysql_connect):
     mock_cursor = Mock()
     mock_cursor.fetchone.return_value = [42]
-    pymysql_mock.cursor.return_value = mock_cursor
+    mock_pymysql_connect.cursor.return_value = mock_cursor
 
-    total_number_polls = get_total_number_polls(pymysql_mock)
+    total_number_polls = get_total_number_polls(mock_pymysql_connect)
 
     assert total_number_polls == 42
 
-def test_get_win_streak(pymysql_mock):
+def test_get_win_streak(mock_pymysql_connect):
     mock_cursor = Mock()
     mock_cursor.fetchone.return_value = [3]  # Simulating 3 days of win streak
-    pymysql_mock.cursor.return_value = mock_cursor
+    mock_pymysql_connect.cursor.return_value = mock_cursor
 
-    win_streak = get_win_streak(pymysql_mock, 'Dog')
+    win_streak = get_win_streak(mock_pymysql_connect, 'Dog')
 
     assert win_streak == 3
 
-def test_get_win_streak_no_results(pymysql_mock):
+def test_get_win_streak_no_results(mock_pymysql_connect):
     mock_cursor = Mock()
     mock_cursor.fetchone.return_value = None  # Simulating no results for win streak
-    pymysql_mock.cursor.return_value = mock_cursor
+    mock_pymysql_connect.cursor.return_value = mock_cursor
 
-    win_streak = get_win_streak(pymysql_mock, 'Cat')
+    win_streak = get_win_streak(mock_pymysql_connect, 'Cat')
 
     assert win_streak == 0
+
+def test_get_cat_or_dog_picture_dog(mock_httpx_client_get):
+    mock_httpx_client_get.return_value.status_code = 200
+    mock_httpx_client_get.return_value.json.return_value = [{'url': 'https://dog.example.com/image.jpg'}]
+
+    dog_url = get_cat_or_dog_picture('fake_dog_api_key', Poll_result.Dog)
+
+    assert dog_url == 'https://dog.example.com/image.jpg'
+
+def test_get_cat_or_dog_picture_cat(mock_httpx_client_get):
+    mock_httpx_client_get.return_value.status_code = 200
+    mock_httpx_client_get.return_value.json.return_value = [{'url': 'https://cat.example.com/image.jpg'}]
+
+    cat_url = get_cat_or_dog_picture('fake_dog_api_key', Poll_result.Cat)
+
+    assert cat_url == 'https://cat.example.com/image.jpg'
+
+def test_upload_picture(mock_requests_get, mock_tweepy_client):
+    mock_requests_get.return_value.status_code = 200
+    mock_requests_get.return_value.iter_content.return_value = [b'some', b'content']
+
+    mock_media = Mock()
+    mock_media.media_id = '1234567890'
+    mock_tweepy_client[1].return_value = mock_media
+
+    media_id = upload_picture("consumer_key", "consumer_secret", "access_token", "access_token_secret",'https://example.com/image.jpg')
+
+    assert media_id == '1234567890'
+
+def test_create_poll(mock_pymysql_connect):
+    mock_cursor = Mock()
+    mock_pymysql_connect.cursor.return_value = mock_cursor
+    mock_cursor.execute.return_value = 0
+
+    create_poll(mock_pymysql_connect, '1234567890')
+
+    assert mock_cursor.execute.called
+
+    expected_call = call('''
+    INSERT INTO Polls (date, poll_id, cat_votes, dog_votes, winner) VALUES (%s, %s, 0, 0, '')
+    ''', (ANY, '1234567890')
+    )
+    
+    mock_cursor.execute.assert_has_calls([expected_call])
 
 # Test the main function
 @patch('os.environ.get', side_effect=lambda key: {
@@ -115,9 +157,9 @@ def test_get_win_streak_no_results(pymysql_mock):
     "DB_USER_PASS": 'fake_pass',
     "DB_NAME": 'fake_name',
     "CERT_PATH": '/fake/path'}.get(key, None)) 
-def test_main_function(mock_env_get, httpx_mock, tweepy_mock, requests_mock, db_mock):
+def test_main_function(mock_env_get, mock_httpx_client_get, mock_tweepy_client, mock_requests_get, mock_db_connect):
     # Mock data and behaviors for httpx
-    httpx_mock.return_value.status_code = 200
+    mock_httpx_client_get.return_value.status_code = 200
     # Define a side effect function that returns different values based on the URL
     def httpx_side_effect(*args, **kwargs):
         mock_response = Mock()
@@ -140,22 +182,22 @@ def test_main_function(mock_env_get, httpx_mock, tweepy_mock, requests_mock, db_
             raise ValueError("Unexpected URL: " + args[0])
 
     # Set up httpx_client.get to use the custom side effect function
-    httpx_mock.side_effect = httpx_side_effect
+    mock_httpx_client_get.side_effect = httpx_side_effect
 
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.iter_content.return_value = [b'some', b'content']
-    requests_mock.return_value = mock_response
+    mock_requests_get.return_value = mock_response
 
     # Mock data and behaviors for tweepy
-    mock_create_tweet, mock_media_upload = tweepy_mock
+    mock_create_tweet, mock_media_upload = mock_tweepy_client
     mock_create_tweet.return_value.data = {'id': '12345'}
     mock_media_upload.return_value.media_id = '67890'
 
     # Mock data and behaviors for pymysql
     mock_cursor = Mock()
     mock_cursor.fetchone.return_value = ["1234567890"]
-    db_mock.return_value.cursor.return_value = mock_cursor
+    mock_db_connect.return_value.cursor.return_value = mock_cursor
 
     # Run the main bot logic
     main()
